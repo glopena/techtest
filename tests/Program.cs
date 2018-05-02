ecbc447bd44281776ae08bf2c32aea09bf9795bd
Glo <glo.pena@outlook.com>
2018-05-02 10:00:59 +0100

    project upload

---
+++ tests/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Calc
+    {
+        public static int multiply(int a, string b)
+        {
+            return a * b;   
+        }
+    }
+}
